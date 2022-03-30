1b6b6c6d09e318e834760e3276df530775e94e75
XtzZZZ <91495193+XtzZZZ@users.noreply.github.com>
2022-03-31 01:07:54 +0300

    Add files via upload

--- PredprofInf-main/App/APpp.py
+++ PredprofInf-main/App/APpp.py
@@ -1,59 +1,63 @@
-import kivy.app as app
+from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition 
 from kivy.core.window import Window
+import os
+from kivy.properties import StringProperty
 
 Window.size = (414, 736)
 
-'''class Main(Screen):
+class All(Screen):
+    month = ' Марта'
+    date = '9'
+    def toTheMain(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="right")
+        self.manager.current = 'Main'
+
+class Main(Screen):
     Username = 'Max'
     Currency = '$'
-    CurrentCurrency = 'Цены указаны в '+ Currency
     Perriod = 'March'
-    Balance = '50000'
-    op1 = 'Бутылка молока'
-    c1 = '-50 р'
-    '''
-    
-
-class Balance(Screen):
-    pass
-
-class Op_Menu(Screen):
-    #cur = 'р'
-    #с1 = '-50' + cur
-    #op = 'Бутылка молока' + с1
-    pass
-
-class Extra(Screen):
-    pass
-
-class AllOp(Screen):
-    pass
+    Balance = 50000
+    StrBalance = str(Balance)
+    cost = '-50 р'
+    operation = 'Бутылка молока'
+    space = '                                     ' 
+    def toTheAll(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'All'
+    def toTheOpMenu(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'OpMenu'
 
 class Costs(Screen):
     pass
 
-class Operation_Change(Screen):
+class OpMenu(Screen):
+    cost = '-50 р'
+    operation = 'Бутылка молока'
+    space = '                                     ' 
+    def toTheMain(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="right")
+        self.manager.current = 'Main'
+        
+
+class Test(Screen):
     pass
 
-class Add_New_Op(Screen):
-    pass
-
-class Op_Menu(Screen):
-    pass
-
-class MoneyCounterApp(app.App):
+class MoneyCounterApp(App):
     
     def build(self):
         sm = ScreenManager()
-        #sm.add_widget(Balance(name='Balance'))
-        #sm.add_widget(Op_Menu(name='Op_Menu'))
-        #sm.add_widget(Operation_Change(name='Operation_Change'))
-        sm.add_widget(Add_New_Op(name='Add_New_Op'))
-        sm.add_widget(AllOp(name='AllOp'))
-        sm.add_widget(Extra(name='Extra'))
+        sm.add_widget(Main(name='Main'))
+        sm.add_widget(OpMenu(name='OpMenu'))
+        sm.add_widget(Test(name='Test'))
+        sm.add_widget(All(name='All'))
         sm.add_widget(Costs(name='Costs'))
         return sm
 
