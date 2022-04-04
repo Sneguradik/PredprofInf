3500e911b331e7f2c701ead24b688be40c02e2b4
Sneguradik <93195734+Sneguradik@users.noreply.github.com>
2022-04-05 00:41:44 +0300

    Add files via upload

--- App/APpp.py
+++ App/APpp.py
@@ -1,28 +1,46 @@
-
+import os
+from posixpath import dirname
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
 import kivy.app as app
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
 from kivy.core.window import Window
 
 Window.size = (414, 736)
 
-class Main(Screen):
+Builder.load_file(os.path.join( dirname(__file__), 'Main.kv'))
+Builder.load_file(os.path.join( dirname(__file__), 'AllTransactions.kv'))
+Builder.load_file(os.path.join( dirname(__file__), 'AddTransaction.kv'))
+
+class Data():
+    pass
+
+class Main(Screen, Data):
     Username = 'Max'
     Currency = '$'
     Perriod = 'March'
     Balance = 50000
     StrBalance = str(Balance)
 
-class Costs(Screen):
+class Costs(Screen, Data):
+    pass
+
+class AllTransactions(Screen, Data):
+    pass
+
+class AddTransaction(Screen , Data):
     pass
 
 class MoneyCounterApp(app.App):
     
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(Main(name='Main'))
-        sm.add_widget(Costs(name='Costs'))
+        #sm.add_widget(Main(name='Main'))
+        #sm.add_widget(AllTransactions(name='AllTransactions'))
+        sm.add_widget(AddTransaction(name='AddTransaction'))
         return sm
 
 
