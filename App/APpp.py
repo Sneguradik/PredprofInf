75d9fda245905ad047a9eee8259bd5e90e521446
XtzZZZ <91495193+XtzZZZ@users.noreply.github.com>
2022-04-11 20:48:07 +0300

    Add files via upload

--- App/APpp.py
+++ App/APpp.py
@@ -1,46 +1,173 @@
-import os
-from posixpath import dirname
-from kivy.uix.widget import Widget
-from kivy.uix.textinput import TextInput
-import kivy.app as app
+from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition 
 from kivy.core.window import Window
+import os
+from kivy.properties import StringProperty
+from kivy.lang import Builder
+from posixpath import dirname
+from kivy.core.text import LabelBase
 
 Window.size = (414, 736)
 
-Builder.load_file(os.path.join( dirname(__file__), 'Main.kv'))
-Builder.load_file(os.path.join( dirname(__file__), 'AllTransactions.kv'))
-Builder.load_file(os.path.join( dirname(__file__), 'AddTransaction.kv'))
+Builder.load_file(os.path.join(dirname(__file__), 'Main.kv'))
+Builder.load_file(os.path.join(dirname(__file__), 'Add.kv'))
+Builder.load_file(os.path.join(dirname(__file__), 'OpMenu.kv'))
+Builder.load_file(os.path.join(dirname(__file__), 'extra.kv'))
+Builder.load_file(os.path.join(dirname(__file__), 'reg.kv'))
+Builder.load_file(os.path.join(dirname(__file__), 'login.kv'))
+Builder.load_file(os.path.join(dirname(__file__), 'AllTransactions.kv'))
 
-class Data():
-    pass
 
-class Main(Screen, Data):
+class All(Screen):
+    month = ' Марта'
+    date = '9'
+    amount = 3
+    amount2 = 5
+    def toTheMain(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="right")
+        self.manager.current = 'Main'
+    def toTheExtra(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'Extra'
+
+class Main(Screen):
     Username = 'Max'
     Currency = '$'
-    Perriod = 'March'
+    Perriod = 'Март'
     Balance = 50000
-    StrBalance = str(Balance)
+    strBalance = str(Balance)
+    cost = '-50 р'
+    operation = 'Бутылка молока'
+    space = '                                     ' 
+    amount = 3
+    amount2 = 5
+    def toTheMain(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="right")
+        self.manager.current = 'Main'
+    def toTheAll(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'All'
+    def toTheOpMenu(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'OpMenu'
+    def toTheExtra(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'Extra'
+    def toTheAdd(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'Add'
 
-class Costs(Screen, Data):
+class Costs(Screen):
     pass
 
-class AllTransactions(Screen, Data):
-    pass
+class OpMenu(Screen):
+    cost = '-50 р'
+    operation = 'Бутылка молока'
+    space = '                                     ' 
+    def toTheMain(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="right")
+        self.manager.current = 'Main'
+    def toTheAll(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'All'
+    def toTheOpMenu(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'OpMenu'
 
-class AddTransaction(Screen , Data):
-    pass
+        
 
-class MoneyCounterApp(app.App):
-    
+class Login(Screen):
+    def do_login(self, loginText, passwordText):
+        app = App.get_running_app()
+        app.username = loginText
+        app.password = passwordText
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'Main'
+
+    def toTheReg(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="right")
+        self.manager.current = 'Reg'
+
+
+    def resetForm(self):
+        self.ids['login'].text = ""
+        self.ids['password'].text = ""
+
+class Reg(Screen):
+    def do_reg(self, loginText, passwordText, password2Text, emailText):
+        app = App.get_running_app()
+        app.username = loginText
+        app.password = passwordText
+        app.password2 = password2Text
+        app.email = emailText
+        c1, c2, c3 = 0, 0, 0
+        for i in emailText:
+            if i == '@':
+                c1 += 1
+            elif i == '#' or i == '$' or i == '*':
+                c3 += 1
+        if c1 != 1 or c3 != 0 or passwordText != password2Text:
+            self.manager.transition = SlideTransition(direction="left")
+            self.manager.current = 'PnP2'
+        else: 
+            self.manager.transition = SlideTransition(direction="left")
+            self.manager.current = 'Main'
+
+class Add(Screen):
+    def toTheMain(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="right")
+        self.manager.current = 'Main'
+    def addNewOp(self, costText, categoryText, dayText, monthText, yearText, nameText):
+        app = App.get_running_app()
+        app.cost = costText
+        app.category = categoryText
+        app.day = dayText
+        app.month = monthText
+        app.year = yearText
+        app.name = nameText
+
+class PnP2(Screen):
+    def toTheReg(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="right")
+        self.manager.current = 'Reg'
+
+class Extra(Screen):
+    def toTheMain(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="right")
+        self.manager.current = 'Main'
+    def toTheExtra(self):
+        app = App.get_running_app()
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'Extra'
+
+class MoneyCounterApp(App):
     def build(self):
         sm = ScreenManager()
-        #sm.add_widget(Main(name='Main'))
-        #sm.add_widget(AllTransactions(name='AllTransactions'))
-        sm.add_widget(AddTransaction(name='AddTransaction'))
+        sm.add_widget(Login(name='Login'))
+        sm.add_widget(Main(name='Main'))
+        sm.add_widget(OpMenu(name='OpMenu'))
+        sm.add_widget(All(name='All'))
+        sm.add_widget(Costs(name='Costs'))
+        sm.add_widget(Reg(name='Reg'))
+        sm.add_widget(PnP2(name='PnP2'))
+        sm.add_widget(Extra(name='Extra'))
+        sm.add_widget(Add(name='Add'))
         return sm
 
 
